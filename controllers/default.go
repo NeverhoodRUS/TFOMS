0a2d171eaf064ef0e091f72b53e4968ed326af07
NeverhoodRUS <john-bursa3@mail.ru>
2023-04-17 18:00:05 +0300

    Refactorimg

--- controllers/default.go
+++ controllers/default.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"tfoms_server/classes/models"
-
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -14,9 +12,5 @@ func (c *MainController) Get() {
 	c.Data["Website"] = "tf.site"
 	c.Data["Email"] = "john-bursa3@mail.ru"
 	c.Data["EmailName"] = "Neverhood"
-	filters := make(map[string]string)
-	filters["organization_id"] = "1"
-	patients, _ := models.GetPatientJournal(filters)
-	c.Data["patients"] = patients
-	c.TplName = "jc.tpl"
+	c.TplName = "index.tpl"
 }
