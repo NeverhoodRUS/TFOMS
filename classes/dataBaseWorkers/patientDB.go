4039fd13f7e4a0425a6e7771ad9c0fa8bf4fcf44
NeverhoodRUS <john-bursa3@mail.ru>
2023-04-17 18:00:58 +0300

    Realised journal filters

--- classes/dataBaseWorkers/patientDB.go
+++ classes/dataBaseWorkers/patientDB.go
@@ -3,7 +3,8 @@ package databaseworkers
 import (
 	"database/sql"
 	"strconv"
-	"tfoms_server/static/strings"
+	"strings"
+	"tfoms_server/static/names"
 )
 
 type PatientColumns struct {
@@ -31,38 +32,108 @@ func GetPatientJournal(filters map[string]string) (*sql.Rows, error) {
 
 func (pc *PatientColumns) GetRowStruct(id int) {
 	var where string = "id = " + strconv.Itoa(id)
-	getRow(strings.PatientTableName, where).Scan(&pc.Id, &pc.ENP, &pc.FirstName, &pc.SecondName, &pc.MiddleName,
+	getRow(names.PatientTableName, where).Scan(&pc.Id, &pc.ENP, &pc.FirstName, &pc.SecondName, &pc.MiddleName,
 		&pc.BirthDate, &pc.Address, &pc.PhoneNumber, &pc.EndInsuranceDate, &pc.InsuranceOrgId, &pc.OrganizationId, &pc.SexId)
 }
 
 func initQueryByFilter(filters map[string]string) string {
+	var result string = "select id from " + names.PatientTableName + " as p where id > 0"
 	if len(filters) == 0 {
-		return "select id from " + strings.PatientTableName + " where id > 0"
+		return result
 	}
-	var result string = "select id from " + strings.PatientTableName + " where "
+
+	setJoinsForFilter(&result, filters)
+	setConditionsForFilter(&result, filters)
+
+	return result
+}
+
+func setConditionsForFilter(result *string, filters map[string]string) {
+	//NOT joined values
 	if val, ok := filters["organization_id"]; ok {
-		result += "organization_id = " + val
+		*result += " and organization_id = " + val
 	}
 	if val, ok := filters["ensurance_org_id"]; ok {
-		result += " and ensurance_org_id = " + val
+		*result += " and ensurance_org_id = " + val
 	}
 	if val, ok := filters["enp"]; ok {
-		result += " and enp = " + val
+		*result += " and enp = " + val
 	}
 	if val, ok := filters["firstname"]; ok {
-		result += " and firstname = " + val
+		*result += " and firstname = " + val
 	}
 	if val, ok := filters["secondname"]; ok {
-		result += " and secondname = " + val
+		*result += " and secondname = " + val
 	}
 	if val, ok := filters["middlename"]; ok {
-		result += " and middlename = " + val
+		*result += " and middlename = " + val
 	}
 	if val, ok := filters["birth_date_before"]; ok {
-		result += " and birthdate <= " + val
+		*result += " and birthdate <= " + val
 	}
 	if val, ok := filters["birth_date_after"]; ok {
-		result += " and birthdate >= " + val
+		*result += " and birthdate >= " + val
 	}
-	return result
+	//JOINED values
+	if _, ok := filters["informing_date"]; ok {
+		*result += " and inf.informing_date is not null"
+	}
+	if val, ok := filters["completed_event_id"]; ok {
+		*result += " and pr.completed_event_id = " + val
+	}
+	if val, ok := filters["planned_year"]; ok {
+		*result += " and ply.planned_year = " + val
+	}
+	if val, ok := filters["planned_month"]; ok {
+		*result += " and ply.planned_month = " + val
+	}
+	if val, ok := filters["priority_group_id"]; ok {
+		*result += "and ply.priority_group_id = " + val
+	}
+	if val, ok := filters["planned_event_id"]; ok {
+		*result += "and ply.planned_event_id = " + val
+	}
+	if _, ok := filters["second_stage"]; ok {
+		*result += " and pr.second_stage is not null"
+	}
+	if val, ok := filters["last_visit_date"]; ok {
+		*result += "and ply.last_visit_date < " + val
+	}
+}
+
+func setJoinsForFilter(result *string, filters map[string]string) {
+	if _, ok := filters["informing_date"]; ok {
+		*result += "join " + names.InformingTableName + " inf on inf.patient_id = p.id"
+	}
+	if _, ok := filters["completed_event_id"]; ok {
+		*result += "join " + names.ProgressTableName + " pr on pr.patient_id = p.id"
+	}
+	if _, ok := filters["planned_year"]; ok {
+		*result += "join " + names.PlanningYearTableName + " ply on ply.patient_id = p.id"
+	}
+	if _, ok := filters["planned_month"]; ok {
+		if ok := strings.Contains(*result, names.PlanningYearTableName); !ok {
+			*result += "join " + names.PlanningYearTableName + " ply on ply.patient_id = p.id"
+		}
+	}
+	if _, ok := filters["priority_group_id"]; ok {
+		if ok := strings.Contains(*result, names.PlanningYearTableName); !ok {
+			*result += "join " + names.PlanningYearTableName + " ply on ply.patient_id = p.id"
+		}
+	}
+	if _, ok := filters["planned_event_id"]; ok {
+		if ok := strings.Contains(*result, names.PlanningYearTableName); !ok {
+			*result += "join " + names.PlanningYearTableName + " ply on ply.patient_id = p.id"
+		}
+	}
+	if _, ok := filters["second_stage"]; ok {
+		if ok := strings.Contains(*result, names.ProgressTableName); !ok {
+			*result += "join " + names.PlanningYearTableName + " pr on pr.patient_id = p.id"
+		}
+	}
+	if _, ok := filters["last_visit_date"]; ok {
+		if ok := strings.Contains(*result, names.PlanningYearTableName); !ok {
+			*result += "join " + names.PlanningYearTableName + " ply on ply.patient_id = p.id"
+		}
+	}
 }
